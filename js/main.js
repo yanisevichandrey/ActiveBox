66a63dc45d8da4d5bd984c58d015e8f3ac1b82f3
andrey.yanisevich <andrey.yanisevich@gmail.com>
2018-02-06 16:29:13 +0200

    Added animations: hover on  the img and the download button

--- js/main.js
+++ js/main.js
@@ -1,19 +1,72 @@
-// window.onscroll = function(){
-//   var countScroll = window.pageYOffset || document.document.Element.scrollTop;
-//   console.log(countScroll);
-//
-//   var headerElem = document.getElementById('header');
-//   if(countScroll > 100){
-//     headerElem.classList.add('fixedHeader');
-//   }else if(headerElem.classList.contains('fixedHeader') && window.pageYOffset < headerElem){
-//     headerElem.classList.remove('fixedHeader');
-//   }
-// }
-$(document).scroll (function(){
-  if($(document).scrollTop () > $('#header').height () + 10){
+new WOW().init();
+$(document).scroll(function () {
+  if ($(document).scrollTop() > $('#header').height() + 10) {
     $('#header').addClass('fixedHeader');
-  }
-  else{
+  } else {
     $('#header').removeClass('fixedHeader');
   }
 });
+
+$('.foto1').hover(
+  function () {
+    $('#wrap1').css('display', 'block');
+  },
+  function () {
+    $('#wrap1').css('display', 'none');
+  });
+
+$('.foto2').hover(
+  function () {
+    $('#wrap2').css('display', 'block');
+  },
+  function () {
+    $('#wrap2').css('display', 'none');
+  });
+
+$('.foto3').hover(
+  function () {
+    $('#wrap3').css('display', 'block');
+  },
+  function () {
+    $('#wrap3').css('display', 'none');
+  });
+
+$('.foto4').hover(
+  function () {
+    $('#wrap4').css('display', 'block');
+  },
+  function () {
+    $('#wrap4').css('display', 'none');
+  });
+
+$('.foto5').hover(
+  function () {
+    $('#wrap5').css('display', 'block');
+  },
+  function () {
+    $('#wrap5').css('display', 'none');
+  });
+
+$('.foto6').hover(
+  function () {
+    $('#wrap6').css('display', 'block');
+  },
+  function () {
+    $('#wrap6').css('display', 'none');
+  });
+
+$('.foto7').hover(
+  function () {
+    $('#wrap7').css('display', 'block');
+  },
+  function () {
+    $('#wrap7').css('display', 'none');
+  });
+
+$('.foto8').hover(
+  function () {
+    $('#wrap8').css('display', 'block');
+  },
+  function () {
+    $('#wrap8').css('display', 'none');
+  });
