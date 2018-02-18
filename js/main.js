88dffeb2b474860cdaef1beb16a57bb415ddb169
Andrey <andrey.yanisevich@gmail.com>
2018-02-19 00:26:00 +0200

    Adaptive menu, fixed bugs and adaptive

--- js/main.js
+++ js/main.js
@@ -70,3 +70,20 @@ $('.foto8').hover(
   function () {
     $('#wrap8').css('display', 'none');
   });
+
+  $(document).ready(function(){
+			var touch = $('.togle');
+		    var menu = $('.nav');
+
+		    $(touch).on('click', function(e) {
+		        e.preventDefault();
+		        menu.slideToggle();
+		    });
+		    $(window).resize(function(){
+		        var wid = $(window).width();
+		        if(wid > 760 && menu.is(':hidden')) {
+		            menu.removeAttr('style');
+		        }
+
+		    });
+		});
